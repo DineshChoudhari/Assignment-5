7588801226cddd191319f38f99361371cc4dcda0
DineshChoudhari <52483550+DineshChoudhari@users.noreply.github.com>
2019-07-10 10:23:36 +0530

    Update Bank_Account.scala

--- src/main/scala/Bank_Account.scala
+++ src/main/scala/Bank_Account.scala
@@ -2,30 +2,38 @@ import scala.io
 import scala.io.StdIn
 
 
-class Bank_Account(var name:String,var acc_no:Int,var balance:Double)
-{
-def display(): Unit ={
-  println(this.name)
-  println(this.acc_no)
-  println(this.balance)
-}
-  def deposite( balance:Double): Unit ={
-  println(this.balance+balance)
-}
-  def withdraw(balance:Double): Unit ={
-    println(this.balance-balance)
+class Bank_Account(var name: String, var acc_no: Int, var balance: Double) {
+  def display(): Unit = {
+    println(this.name)
+    println(this.acc_no)
+    println(this.balance)
   }
-};
+
+  def deposite(balance: Double): Unit = {
+    println(this.balance + balance)
+  }
+
+  def withdraw(balance: Double): Unit = {
+    println(this.balance - balance)
+  }
+
+  def printObject() = {println(this)
+   // Bank_Account { delete from "pri" objectId "myId" }
+
+  }
+
 object Bank_Account {
+  //implicit var balance:Double=5000
   def main(args: Array[String]): Unit = {
-    var bank_acc=new Bank_Account()
-    var choice = StdIn.readLine("Enter choice")
+    var choice = StdIn.readLine("Enter choice").toInt
+    var bank_acc = new Bank_Account("dinesh", 123, 5000)
+    bank_acc.printObject()
     choice match {
-      case 1 =>var bank_acc = new Bank_Account ("dinesh", 123, 123)
-      case 2=> bank_acc.display ()
-      case 3=>bank_acc.deposite (300)
-      case 4=>bank_acc.withdraw (100)
-//      case 5=>bank_acc.fundtranfer()
+
+      case 1 => bank_acc.display()
+      case 2 => bank_acc.deposite(300)
+      case 3 => bank_acc.withdraw(100)
+      case 4 => bank_acc.printObject()
     }
 
   }
